ab220b4899ff98864ac7f2f38b63cf141c65d168
jon <pivaq4530@gmail.com>
2025-09-02 19:21:40 +0300

    Worked on /set

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from bot.Lexicon.lexica import help_info
 from bot.config.user_info import user_data
 from bot.Keyboards.get_board import get_board
@@ -17,7 +17,7 @@ async def start(msg: Message) -> None:
     main()
     await msg.reply(f'Hello, {msg.from_user.username}!')
     if msg.from_user.id not in user_data:
-        user_data[msg.from_user.id] = {msg.from_user.username, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.is_premium}
+        user_data[msg.from_user.id] = [msg.from_user.username, msg.from_user.first_name, msg.from_user.last_name, msg.from_user.is_premium]
     print(user_data)
 
 
@@ -41,7 +41,20 @@ async def send_user_info(msg: Message) -> None:
     await msg.reply(anw_info.create())
 
 
+# Send board to choose favorite
 @router.message(Command(commands=['set']))
-async def set_favorite_one(msg: Message) -> None:
+async def user_choose_favorite_one(msg: Message) -> None:
     await msg.reply("Choose: ", reply_markup=set_board)
+
+
+# Send user info about his loved one
+@router.callback_query()
+async def set_favorite(msg: CallbackQuery) -> None:
+    setted_favorite = msg.data
+    if setted_favorite not in user_data:
+        user_data[msg.from_user.id].append(setted_favorite)
+    await msg.message.reply(follow_favourite_currency(msg.data))
+
+
+
 
